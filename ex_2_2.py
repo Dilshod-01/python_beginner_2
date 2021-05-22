df949e9f8e529481bce52628eac6e84a5db39ad1
Dilshod-01 <valiyevdilshod99@gmail.com>
2021-05-22 11:44:01 +0500

    2

---
+++ ex_2_2.py
@@ -0,0 +1,62 @@
+# fruits = [
+#     'apple',
+#     'banana',
+#     'dragon fruit',
+#     'mango',
+#     'nectarine',
+#     'pear',
+# ]
+# print('our fruit selection:')
+# for fruit in fruits:
+#     print(fruit)
+#A = input("xonaning bo'yini kiriting: ")
+#B = input("xonaning enini kiriting: ")
+
+#A = int (A)
+#B = int (B)
+
+#C = (A**3 )
+#print("sizning xonangizning kubi: ",C)
+
+#def kub(x):
+ #   return x**3
+
+# C =input("raqam kiriting: ", )
+# print(C)
+#kub=[x*x*x for x in range(1,10)]
+
+
+# x = input("son kiriting: ")
+# y = input("raqam kiriting: ")
+
+# x = int (x)
+# y = int (y)
+
+# a = (x + y )
+# print("yig'indi: ", a +10)
+
+# ism = "Dilshod"
+# yosh = "20"
+# print(ism + yosh)
+
+# a = input("Ismingizni kiriting: ")
+# b = input("Yoshingizni kiriting: ")
+
+# a = int (a)b
+# b = int (b)
+
+b = "23"
+c = "5"
+
+g = b+c
+print(type(g))
+
+
+x = int (b + c)
+print(type(x))
+
+
+
+
+
+
